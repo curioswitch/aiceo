a104bb574eb32115a4cdb976b40f99f2c6880888
Choko <choko@curioswitch.org>
2024-11-20 17:13:31 +0900

    Load noto sans JP font

--- client/src/layouts/Head.tsx
+++ client/src/layouts/Head.tsx
@@ -8,6 +8,16 @@ export default function Head() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="A matching service with CEOs." />
       <link rel="icon" href={logoUrl} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap"
+        rel="stylesheet"
+      />
     </>
   );
 }
