2192ba773bf488c211a4a1ac1b1904f8be4279c8
Choko <choko@curioswitch.org>
2025-02-19 22:28:42 +0900

    Update vite and reduce button size

--- client/src/pages/index/+Page.tsx
+++ client/src/pages/index/+Page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
         </div>
         <Spacer y={10} />
         <Button
-          className="bg-primary text-white text-lg font-medium w-full md:w-2/3 min-h-12 md:min-h-16"
+          className="bg-primary text-white text-lg font-medium w-full md:w-2/3 min-h-12"
           radius="sm"
           onPress={onStart}
           isDisabled={doStart.isPending}
@@ -73,7 +73,7 @@ export default function Page() {
           スタート！
         </Button>
         <Button
-          className="bg-white text-lg font-medium w-full md:w-2/3 min-h-12 md:min-h-16 text-black"
+          className="bg-white text-lg font-medium w-full md:w-2/3 min-h-12 text-black"
           radius="sm"
           fullWidth
           onPress={onPast}
